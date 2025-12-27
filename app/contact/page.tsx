648417c925e77a1e91584b88733f72965c31510f
JuanLopez2004 <jlopeze22004@gmail.com>
2025-12-27 15:23:20 -0600

    pathways, imagehosting, and logo

---
+++ app/contact/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import Background from '@/components/background';
+import Header from '@/components/header';
+import Hero from '@/components/hero';
+import Footer from '@/components/footer';
+
+export default function Home() {
+  return (
+    <>
+      <Background />
+      <Header />
+      <Hero />
+      <Footer />
+    </>
+  )
+}
--- components/header.tsx
+++ components/header.tsx
@@ -7,10 +7,17 @@ export default function Header() {
             <header className={styles.header}>
                 <div className={styles.headerContainer}>
                     <div className={styles.headerLeft}>
+                         <Link href="/">
+                            <img
+                                src="https://github.com/user-attachments/assets/75ba5dcd-94c9-4eba-b13d-ce46536f9bee"
+                                alt="Logo"
+                                className={styles.headerLogo}
+                            />
+                        </Link>
                         <span className={styles.headerTitle}>goosefm</span>
                     </div>
                     <nav className={styles.headerNav}>
-                        <Link href="/app">Home</Link>
+                        <Link href="/">Home</Link>
                         <Link href="/#About">About</Link>
                         <Link href="/radio">Radio</Link>
                         <Link href="/schedule">Schedule</Link>
